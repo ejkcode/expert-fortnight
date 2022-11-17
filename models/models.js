a501e8acf03b991b7c97facb33720804ad8b9219
ejkcode <elizabeth.kelly28@nhs.net>
2022-11-17 12:05:18 +0000

    10-GET-review-by-id-with-commentCount

--- models/models.js
+++ models/models.js
@@ -23,8 +23,11 @@ const fetchReviews = () => {
 
 const fetchReviewById = (review_id) => {
     return db.query(`
-        SELECT * FROM reviews
-        WHERE review_id = $1;
+        SELECT reviews.*, COUNT(comments.review_id) AS comment_count
+        FROM reviews
+        LEFT JOIN comments ON comments.review_id = reviews.review_id
+        WHERE reviews.review_id = $1
+        GROUP BY reviews.review_id
     `, [review_id])
     .then((result) => {
         if (result.rows.length === 0){
