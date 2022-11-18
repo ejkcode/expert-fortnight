648156cef8892a2b1c4b1a8f28fd71ee3f535289
ejkcode <elizabeth.kelly28@nhs.net>
2022-11-18 10:27:37 +0000

    12-DELETE-comment-by-id

--- models/models.js
+++ models/models.js
@@ -126,5 +126,19 @@ const fetchUsers = () => {
         });
 };
 
-module.exports = {fetchCategories, fetchReviews, fetchReviewById, fetchCommentByReviewId, addCommentByReviewId, updateReviewById, fetchUsers};
+const removeCommentById = (comment_id) => {
+    return db.query(`
+        DELETE FROM comments
+        WHERE comment_id = $1
+        RETURNING *;
+    `, [comment_id])
+    .then((result) => {
+        if (result.rows.length === 0){
+            return Promise.reject({status: 404, msg: 'comment_id not found'});
+        };
+        return result.rows[0];
+    });
+};
+
+module.exports = {fetchCategories, fetchReviews, fetchReviewById, fetchCommentByReviewId, addCommentByReviewId, updateReviewById, fetchUsers, removeCommentById};
 
